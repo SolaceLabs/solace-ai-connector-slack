2b3cba7b11a31a31b01b1657b408d97c1f61b3bc
matteopolak <matthew.polak@solace.com>
2025-03-18 15:53:14 -0400

    feat: add threads

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,40 +1,52 @@
 import os
-import discord
-from discord.ext import commands
+from discord import Intents, Client, Interaction, Message, ChannelType, ButtonStyle, InteractionType
 from discord.ui import Button, View
 
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+  print(f'We have logged in as {client.user}')
 
-async def thumbsup_callback(interaction: discord.Interaction):
-    await interaction.response.send_message("You clicked thumbs up!", ephemeral=True)
+async def thumbsup_callback(interaction: Interaction):
+  await interaction.response.send_message("You clicked thumbs up!", ephemeral=True)
 
-async def thumbsdown_callback(interaction: discord.Interaction):
-    await interaction.response.send_message("You clicked thumbs down!", ephemeral=True)
-
+async def thumbsdown_callback(interaction: Interaction):
+  await interaction.response.send_message("You clicked thumbs down!", ephemeral=True)
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_interaction(interaction: Interaction):
+  if interaction.type != InteractionType.component:
+    return
 
-    if client.user.mentioned_in(message):
-        thumbsup_button = Button(label="👍", style=discord.ButtonStyle.green)
-        thumbsdown_button = Button(label="👎", style=discord.ButtonStyle.red)
-        
-        thumbsup_button.callback = thumbsup_callback
-        thumbsdown_button.callback = thumbsdown_callback
-        view = View()
-        view.add_item(thumbsup_button)
-        view.add_item(thumbsdown_button)
-        
-        # Send the message with the buttons
-        await message.channel.send("test", view=view)
+@client.event
+async def on_message(message: Message):
+  if message.author == client.user or not client.user:
+    return
 
-client.run(os.getenv('DISCORD_TOKEN'))
+  if not client.user.mentioned_in(message):
+    return
+
+  thumbsup_button = Button(label="👍", style=ButtonStyle.green)
+  thumbsdown_button = Button(label="👎", style=ButtonStyle.red)
+
+  thumbsup_button.callback = thumbsup_callback
+  thumbsdown_button.callback = thumbsdown_callback
+
+  view = View()
+  view.add_item(thumbsup_button)
+  view.add_item(thumbsdown_button)
+
+  for attachment in message.attachments:
+    attachment.url
+
+  if message.channel.type in [ChannelType.public_thread, ChannelType.private_thread]:
+    await message.reply("hello world", view=view)
+  else:
+    thread = await message.create_thread(name="solly")
+    await thread.send("hello world", view=view)
+
+client.run(os.getenv('DISCORD_TOKEN') or exit(-123))
