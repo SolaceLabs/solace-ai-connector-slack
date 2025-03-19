f6c9d0fb03538326383c2ac1f274c493e3a52dd1
matteopolak <matthew.polak@solace.com>
2025-03-19 10:50:21 -0400

    chore: bring back interaction handler

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,20 +1,12 @@
 import os
-from discord import Intents, Client, Interaction, Message, ChannelType, ButtonStyle, InteractionType, ComponentType
+from discord import Intents, Interaction, Message, ChannelType, ButtonStyle, InteractionType, ComponentType
 from discord.ui import Button, View
-from discord.ext import commands
+from discord.ext.commands import Bot
 
-intents = Intents.default()
-intents.dm_messages = True
-intents.guild_messages = True
-
-client = Client(intents=intents)
+client = Bot(command_prefix="!", intents=Intents.default())
 
 def trunc(text: str, max: int = 20):
   return text[:max] if len(text) > max else text
-
-@client.event
-async def on_ready():
-  print(f'We have logged in as {client.user}')
 
 async def thumbs_up(interaction: Interaction):
   await interaction.response.send_message("You clicked thumbs up!", ephemeral=True)
@@ -42,8 +34,10 @@ async def on_message(message: Message):
   print("got msg")
   if message.author == client.user or not client.user:
     return
+  
+  is_dm = message.guild is None
 
-  if not client.user.mentioned_in(message):
+  if not is_dm and not client.user.mentioned_in(message):
     return
 
   thumbsup_button = Button(label="👍", style=ButtonStyle.green, custom_id="thumbs_up")
@@ -56,7 +50,7 @@ async def on_message(message: Message):
   for attachment in message.attachments:
     attachment.url
 
-  if message.channel.type in [ChannelType.public_thread, ChannelType.private_thread]:
+  if is_dm or message.channel.type in [ChannelType.public_thread, ChannelType.private_thread]:
     await message.reply("hello world", view=view)
   else:
     thread = await message.create_thread(name=trunc(message.clean_content))
@@ -64,15 +58,15 @@ async def on_message(message: Message):
 
 @client.event
 async def on_ready():
+  print("hello")
   try:
     s = await client.tree.sync()
     print(f"Synced {len(s)} commands")
   except Exception as e:
     print(e)
 
-
 @client.tree.command(name = "help")
-async def test(interaction: discord.Interaction):
+async def help(interaction: Interaction):
   await interaction.response.send_message(f"""
 Hi {interaction.user.mention}!
 
@@ -89,6 +83,5 @@ I'm the Solace AI Chatbot, designed to assist Solace employees with various task
 * Search the web for current information
 
 I can help with both general inquiries and Solace-specific questions by leveraging specialized agents to access the right information sources. Just let me know what you need assistance with!""", ephemeral=True)
-
 
 client.run(os.getenv('DISCORD_TOKEN') or exit(-123))
